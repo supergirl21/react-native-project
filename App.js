c88c73a3c96c344114ee7e5087d5fd57911bbd51
supergirl <supergirl@anniecannons.com>
2021-07-03 23:43:54 -0700

    Second commit to add all the codes

--- App.js
+++ App.js
@@ -1,21 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
+
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import ViewImageScreen from './app/screens/ViewImageScreen';
+import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>I am Supergirl.</Text>
-      <StatusBar style="auto" />
-    </View>
+    <WelcomeScreen />,
+    <ViewImageScreen />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
